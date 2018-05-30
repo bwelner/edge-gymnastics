728bbd5ab940e35f383a9b3c6ca31aaf3c702c77
Brandon M. Welner <brandonmaxwelner@gmail.com>
2018-05-30 15:52:42 -0700

    First Commit, some routing

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { CoachesComponent } from './components/coaches/coaches.component';
+import { ClassesComponent } from './components/classes/classes.component';
+import { SessionsComponent } from './components/sessions/sessions.component';
+import { TeamComponent } from './components/team/team.component';
+import { PartiesComponent } from './components/parties/parties.component';
+import { FacilityComponent } from './components/facility/facility.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    CalendarComponent,
+    CoachesComponent,
+    ClassesComponent,
+    SessionsComponent,
+    TeamComponent,
+    PartiesComponent,
+    FacilityComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
