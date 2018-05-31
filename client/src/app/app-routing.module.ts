0653388e6341e12a9ff3364b887f5b459dc9ca1a
Brandon M. Welner <brandonmaxwelner@gmail.com>
2018-05-30 17:36:22 -0700

    added more components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 import { AboutComponent } from './components/about/about.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { ClassesComponent } from './components/classes/classes.component';
+import { CoachesComponent } from './components/coaches/coaches.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PartiesComponent } from './components/parties/parties.component';
+import { SessionsComponent } from './components/sessions/sessions.component';
+import { TeamComponent } from './components/team/team.component';
 
 const routes: Routes = [
   {
@@ -11,9 +18,37 @@ const routes: Routes = [
     pathMatch: 'full'
     },
     {
-    path: 'about',
-      component: AboutComponent
+    path: 'calendar',
+      component: CalendarComponent
     },
+    {
+      path: 'classes',
+        component: ClassesComponent
+    },
+    {
+      path: 'coaches',
+          component: CoachesComponent
+    },
+    {
+      path: 'contact',
+        component: ContactComponent
+      },
+    {
+        path: 'parties',
+          component: PartiesComponent
+    },
+    {
+      path: 'sessions',
+        component: SessionsComponent
+      },
+      {
+        path: 'team',
+          component: TeamComponent
+        },
+    {
+      path: 'about',
+        component: AboutComponent
+      },
     {
     path: '**', redirectTo: ''
     }
